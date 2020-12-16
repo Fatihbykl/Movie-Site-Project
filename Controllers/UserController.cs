2ed909612eb96da4dcfe9c0bb7297042979ed24f
Fatihbykl <41392755+Fatihbykl@users.noreply.github.com>
2020-12-16 18:42:00 +0300

    init

---
+++ Controllers/UserController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebProgramlamaProje.Models;
+using Microsoft.AspNetCore.Http;
+using WebProgramlamaProje.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace WebProgramlamaProje.Controllers
+{
+    
+    public class UserController : Controller
+    {
+        private UserManager<User> userManager;
+        private SignInManager<User> signinManager;
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(UserManager<User> context, SignInManager<User> signin, ILogger<UserController> logger)
+        {
+            userManager = context;
+            signinManager = signin;
+            _logger = logger;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Signup(RegisterModel model)
+        {
+            if (User.Identity.IsAuthenticated) { return Redirect("../Home/Index"); }
+
+            if (ModelState.IsValid)
+            {
+                User user = new User()
+                {
+                    Name = model.Name,
+                    UserName = model.UserName,
+                    Email = model.Email,
+                };
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Signin");
+                }
+                else
+                {
+                    foreach (var i in result.Errors)
+                    {
+                        ModelState.AddModelError("", i.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Signup()
+        {
+            if (User.Identity.IsAuthenticated) { return Redirect("../Home/Index"); }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Signin()
+        {
+            if (User.Identity.IsAuthenticated) { return Redirect("../Home/Index"); }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Signin(LoginModel model)
+        {
+            if (User.Identity.IsAuthenticated) { return Redirect("../Home/Index"); }
+
+            Console.WriteLine("asdsadsad - " + ModelState.IsValid);
+            if (ModelState.IsValid)
+            {
+                var result = await signinManager.PasswordSignInAsync(model.UserName, model.Password, true, lockoutOnFailure: false);
+                Console.WriteLine("result  " + result);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User logged in.");
+                    return Redirect("../Home/Index");
+                }
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await signinManager.SignOutAsync();
+            _logger.LogInformation("User logged out.");
+            return Redirect("../Home/Index");//Homepage git.
+        }
+    }
+}
---
+++ Models/RegisterModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProgramlamaProje.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [MaxLength(25, ErrorMessage = "En fazla 25 harf girilebilir.")]
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [MaxLength(30, ErrorMessage = "En fazla 30 harf girilebilir.")]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
